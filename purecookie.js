403004420ce8cd14dc297f6ac106ace60514acee
Marcus Kamuntu <63915967+aspekts@users.noreply.github.com>
2022-09-15 19:22:45 +0000

    Initial commit

---
+++ purecookie.js
@@ -0,0 +1,11 @@
+// popup title
+var purecookieTitle = "Cookies."; 
+
+// popup content
+var purecookieDesc = "By using this website, you automatically accept that we use cookies.";
+
+// policy link
+var purecookieLink = '<a href="cookie-policy.html" target="_blank">What for?</a>'; 
+
+// button text
+var purecookieButton = "Understood";
